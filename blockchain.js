7b75f26a409c53074d7a85b2b7ce64292930f6e1
Christian Zamora <67123381+Zamorocks@users.noreply.github.com>
2021-11-01 23:50:53 -0700

    first commit

---
+++ blockchain.js
@@ -0,0 +1,50 @@
+const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
+
+class Blockchain {
+    constructor(){
+        this.chain = [Block.genesis()];
+    }
+
+    addBlock({ data }) {
+        const newBlock = Block.mineBlock({
+            lastBlock: this.chain[this.chain.length-1],
+            data
+        });
+        this.chain.push(newBlock);
+    }
+
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        };
+        for (let i = 1; i < chain.length; i++) {
+            const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
+
+            const actualLastHash = chain[i-1].hash;
+            
+            if (lastHash !== actualLastHash) return false;
+            
+            const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+            
+            if (hash !== validatedHash) return false;
+        }
+        return true;
+    }
+
+    replaceChain(chain) {
+        if (chain.length <= this.chain.length) {
+            console.error('new chain not longer than the older chain');
+            return;
+        }
+
+        if (!Blockchain.isValidChain(chain)) {
+            console.error('new chain not longer than the older chain');
+            return;
+        }
+        console.log('replacing chain with', chain);
+        this.chain = chain;
+    }
+}
+
+module.exports = Blockchain;
